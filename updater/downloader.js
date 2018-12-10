f6200b7770fd0c84ba0ce93704255432174d8161
philipplgh <philipplgh@gmail.com>
2018-12-10 19:55:46 +0100

    allow download to memory/buffer with progress

--- updater/downloader.js
+++ updater/downloader.js
@@ -1,9 +1,10 @@
 const http = require('http')
 const https = require('https')
+const stream = require('stream')
 
 const axios = require('axios')
 
-function download(url, w, progress = () => {}) {
+function downloadRaw(url, w, progress = () => {}) {
   return new Promise((resolve, reject) => {
     let protocol = /^https:/.exec(url) ? https : http;
     progress(0);
@@ -12,7 +13,7 @@ function download(url, w, progress = () => {}) {
         protocol = /^https:/.exec(res1.headers.location) ? https : http;
         protocol
           .get(res1.headers.location, res2 => {
-            const total = parseInt(res2.headers['content-length'], 10);
+            const total = parseInt(res2.headers['content-length'], 0);
             let completed = 0;
             res2.pipe(w);
             res2.on('data', data => {
@@ -29,13 +30,48 @@ function download(url, w, progress = () => {}) {
   });
 }
 
+async function downloadBinAxios(url) {
+  let response = await axios.get(url, {
+    responseType: 'arraybuffer',
+    // not working!
+    // https://github.com/axios/axios/issues/928#issuecomment-322053374
+    onDownloadProgress: (progressEvent) => {
+      console.log('progress', progressEvent)
+    }
+  })
+  return new Buffer.from(response.data, 'binary')
+}
+
+class WMStrm extends stream.Writable {
+  constructor(){
+    super()
+    this.buffer
+  }
+  _write (chunk, enc, cb) {
+    var buffer = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk, enc);
+    if(this.buffer === undefined) {
+      this.buffer = buffer
+    } else {
+      this.buffer = Buffer.concat([this.buffer, buffer]);
+    }
+    cb()
+  }
+}
+
+async function download(url, onDownloadProgress) {
+  const memoryBuffer = new WMStrm()
+  await downloadRaw(url, memoryBuffer, onDownloadProgress)
+  return memoryBuffer.buffer
+}
+
 async function downloadJson(url){
   let response = await axios.get(url)
   return response.data
 }
 
-function downloadToFile(){
-
+function downloadToFile(filePath){
+  // const dest = fso.createWriteStream(filePath);
+  // downloadRaw(url, dest) 
 }
 
 module.exports.download = download
