b578ca609988d19e4c312a926d0b07b4ac4bd507
philipplgh <philipplgh@gmail.com>
2018-12-11 19:05:37 +0100

    remove axios

--- updater/downloader.js
+++ updater/downloader.js
@@ -2,34 +2,39 @@ const http = require('http')
 const https = require('https')
 const stream = require('stream')
 
-const axios = require('axios')
-
 function downloadRaw(url, w, progress = () => {}) {
   return new Promise((resolve, reject) => {
     let protocol = /^https:/.exec(url) ? https : http;
     progress(0);
+
+    const _download = (res) => {
+      const total = parseInt(res.headers['content-length'], 0);
+      let completed = 0;
+      res.pipe(w);
+      res.on('data', data => {
+        completed += data.length;
+        progress(completed / total);
+      });
+      res.on('progress', progress);
+      res.on('error', reject);
+      res.on('end', () => resolve(w.path));
+    }
+
     protocol
       .get(url, res1 => {
-        protocol = /^https:/.exec(res1.headers.location) ? https : http;
-        protocol
-          .get(res1.headers.location, res2 => {
-            const total = parseInt(res2.headers['content-length'], 0);
-            let completed = 0;
-            res2.pipe(w);
-            res2.on('data', data => {
-              completed += data.length;
-              progress(completed / total);
-            });
-            res2.on('progress', progress);
-            res2.on('error', reject);
-            res2.on('end', () => resolve(w.path));
-          })
-          .on('error', reject);
+        // if redirect -> follow
+        if(res1.headers.location){
+          protocol = /^https:/.exec(res1.headers.location) ? https : http;
+          protocol.get(res1.headers.location, res2 => _download(res2)).on('error', reject);
+        }else{
+          _download(res1)
+        }
       })
       .on('error', reject);
   });
 }
 
+/*
 async function downloadBinAxios(url) {
   let response = await axios.get(url, {
     responseType: 'arraybuffer',
@@ -41,6 +46,7 @@ async function downloadBinAxios(url) {
   })
   return new Buffer.from(response.data, 'binary')
 }
+*/
 
 class WMStrm extends stream.Writable {
   constructor(){
