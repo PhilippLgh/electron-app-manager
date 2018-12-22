1b1c3dffa34c639e50619fde8dd3c52e781d1bb0
philipplgh <philipplgh@gmail.com>
2018-12-22 16:15:44 +0100

    refactoring

--- updater/ElectronDialogUpdater.js
+++ updater/ElectronDialogUpdater.js
@@ -1,69 +1,111 @@
 const Updater = require('./updater')
 const { autoUpdater, CancellationToken } = require("electron-updater")
-
 const { dialog } = require('electron')
+
+const Dialog = {
+  displayUpToDateDialog : () =>{
+    dialog.showMessageBox({
+      title: 'No update',
+      message: 'You are using the latest version'
+    })
+  },
+
+  displayUpdateFoundDialog : (update, callback) => {
+    dialog.showMessageBox({
+        title: 'Checking for updates',
+        message: `
+          Update found: ${update.fileName} v${update.version} 
+          Press "OK" to download in background
+          `
+    }, (response) => {
+      // response: Number - The index of the button that was clicked
+      console.log('user response to update:', response)
+    });
+  },
+
+  displayRestartForUpdateDialog : (update) => {
+    updater.on('update-downloaded', () => {
+      dialog.showMessageBox({
+        title: 'Install Updates',
+        message: 'Updates downloaded, application will be quit for update...'
+      }, () => {
+        setImmediate(() => autoUpdater.quitAndInstall())
+      })
+    })
+  },
+
+  displayReloadForUpdateDialog : (update) => {
+    dialog.showMessageBox({
+      title: 'Install Updates',
+      message: `Press OK to reload for an update to version ${update.version}`
+    }, () => {
+      console.log('reload not yet implemented')
+    })
+  },
+
+  displayDownloadFailedDialog : (download) => {
+    dialog.showMessageBox({
+      title: 'Download failed',
+      message: `Error ${download.error}`
+    })
+  },
+
+  displayErrorDialog : (error) => {
+    dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
+  }
+}
 
 class ElectronUpdater extends Updater {
   constructor(opts) {
     super(opts)
 
-    // TODO remove listeners?
-
-    let updater = this
     if(opts.shell === true) {
-      updater = autoUpdater
       this.isShell = true
     }
 
-    updater.on('update-available', this.handleUpdateAvailable)
-    updater.on('update-not-available', () => {
-      dialog.showMessageBox({
-        title: 'No Updates',
-        message: 'Current version is up-to-date.'
-      })
-      this.updateMenuBtn.enabled = true
-      this.updateMenuBtn = null
-    })
-    updater.on('error', (error) => {
-      dialog.showErrorBox('Error: ', error == null ? "unknown" : (error.stack || error).toString())
-      this.updateMenuBtn.enabled = true
-      this.updateMenuBtn = null
-    })
-
-    if(this.isShell) {
-      updater.on('update-downloaded', () => {
-        dialog.showMessageBox({
-          title: 'Install Updates',
-          message: 'Updates downloaded, application will be quit for update...'
-        }, () => {
-          setImmediate(() => autoUpdater.quitAndInstall())
-        })
-      })
-    } else {
-      // needs to be handled in app
-      //TODO is different for UI and background script changes
-      updater.on('update-downloaded', () => {
-        dialog.showMessageBox({
-          title: 'Install Updates',
-          message: 'Updates downloaded, reload to see changes...'
-        }, () => {
-
-        })
-      })
-    }
-
-    this.handleUpdateAvailable = this.handleUpdateAvailable.bind(this)
     this.checkForUpdates = this.checkForUpdates.bind(this)
   }
 
-  handleUpdateAvailable(update) {
-    dialog.showMessageBox({
-      type: 'info',
-      title: 'Found Updates',
-      message: 'Found updates, do you want update now?',
-      buttons: ['Sure', 'No']
-    }, (buttonIndex) => {
-      if (buttonIndex === 0) {
+  async download(update, silent){
+    update = await super.download(update)
+    if(silent) {
+      return update
+    }
+    if(this.isShell) {
+      Dialog.displayRestartForUpdateDialog()
+    } else {
+      if (!update.error) { 
+        Dialog.displayReloadForUpdateDialog(update)
+      } else {
+        Dialog.displayDownloadFailedDialog()
+      }
+    }
+    return update
+  }
+
+  async checkForUpdates() {
+
+    if(this.isShell){
+      autoUpdater.checkForUpdates()
+    } 
+
+    let update
+    try {
+      update = await super.checkForUpdates()
+    } catch (error) {
+      Dialog.displayErrorDialog(error)
+      return
+    }
+
+    // no update found
+    if (!update) {
+      Dialog.displayUpToDateDialog()
+      return;
+    }
+
+    // update found
+    Dialog.displayUpdateFoundDialog(update, decision => {
+      if (decision === 'download') {
         if(this.isShell){
           const cancellationToken = new CancellationToken()
           autoUpdater.downloadUpdate(cancellationToken)
@@ -71,23 +113,8 @@ class ElectronUpdater extends Updater {
           this.download(update)
         }
       }
-      else {
-        this.updateMenuBtn.enabled = true
-        this.updateMenuBtn = null
-      }
     })
   }
-
-  checkForUpdates(menuItem, focusedWindow, event) {
-    this.updateMenuBtn = menuItem
-    this.updateMenuBtn.enabled = false
-    if(this.isShell){
-      autoUpdater.checkForUpdates()
-    } else {
-      super.checkForUpdates()
-    }
-  }
-
 }
 
 module.exports = ElectronUpdater
