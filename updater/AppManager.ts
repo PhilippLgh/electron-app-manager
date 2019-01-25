5b09b47fcb39ae074137b791a9871efaef360725
PhilippLgh <philipp@ethereum.org>
2019-01-25 16:31:20 +0100

    move HotLoading to own module

--- updater/AppManager.ts
+++ updater/AppManager.ts
@@ -3,32 +3,42 @@ import { IRemoteRepository } from './api/IRepository'
 import GithubRepo from './repositories/Github'
 import AzureRepo from './repositories/Azure'
 import Cache from './repositories/Cache'
-import { IRelease } from './api/IRelease'
+import { IRelease, IReleaseExtended } from './api/IRelease'
 import fs from 'fs'
 import path from 'path'
+import semver from 'semver';
+import HotLoader from './HotLoader';
 
 interface IUpdaterOptions {
   repository: string;
+  auto?: boolean;
+  intervalMins?: number,
   cacheDir?: string;
   downloadDir?: string;
   modifiers? : { [key : string] : Function }
+  filter?: Function
 }
 
 export default class AppManager extends EventEmitter{
   
   remote: IRemoteRepository;
   cache: Cache;
+  checkHandler: any; // IntervalHandler
   
   /**
    *
    */
-  constructor({ repository, cacheDir, downloadDir, modifiers } : IUpdaterOptions) {
+  constructor({ repository, auto = true, intervalMins = 15, cacheDir, modifiers, filter } : IUpdaterOptions) {
     super();
 
     if(repository.startsWith('https://github.com/')) {
       this.remote = new GithubRepo(repository)
     }
     else if(repository.includes('blob.core.windows.net')){
+
+      // FIXME check that only host name provided or parse
+      repository += '/builds?restype=container&comp=list'
+
       if(modifiers){
         let mod = (release : IRelease) => {
           let result : {[key:string] : any} = { }
@@ -38,7 +48,8 @@ export default class AppManager extends EventEmitter{
           return result
         }
         this.remote = new AzureRepo(repository, {
-          onReleaseParsed: mod
+          onReleaseParsed: mod,
+          filter
         })      
       } else {
         this.remote = new AzureRepo(repository)
@@ -54,19 +65,84 @@ export default class AppManager extends EventEmitter{
       this.cache = new Cache(__dirname)
     }
 
+    this.checkForUpdates = this.checkForUpdates.bind(this)
+
+    if(auto){
+      if (intervalMins <= 5 || intervalMins > (24 * 60)) {
+        throw new Error(`Interval ${intervalMins} (min) is unreasonable or not within api limits`)
+      }
+      let intervalMs = intervalMins * 60 * 1000
+      // FIXME this.startUpdateRoutine(intervalMs)
+    }
+
+  }
+
+  get repository() : string{
+    return this.remote.repositoryUrl
+  }
+
+  private startUpdateRoutine(intervalMs : number){
+    if (this.checkHandler) {
+      throw new Error('Update routine was started multiple times')
+    }
+    let errorCounter = 0
+
+    const check = async () => {
+      let latest = await this.checkForUpdates()
+      if (latest && latest.version) {
+        console.log('update found: downloading ', latest.version);
+        try {
+          let download = await this.download(latest)
+
+        } catch (error) {
+          errorCounter++
+          console.error(error)
+        }
+      }
+    }
+
+    check()
+    this.checkHandler = setInterval(check, intervalMs)
+  }
+
+  async checkForUpdates() : Promise<IRelease | IReleaseExtended | null> {
+    const latestCached = await this.cache.getLatest()
+    const latestRemote = await this.remote.getLatest()
+    if(!latestCached){
+      return latestRemote // null || release
+    }
+    if(!latestRemote){
+      return null
+    }
+    if(semver.gt(latestRemote.version, latestCached.version)){
+      return latestRemote
+    }
+    return null 
   }
 
   async getLatestCached(){
     return this.cache.getLatest()
   }
 
+  async getLatestRemote(){
+    return await this.remote.getLatest()
+  }
+
   async getLatest() : Promise<IRelease | null>{
-    let latestCached = await this.cache.getLatest()
-    let latestRemote = await this.remote.getLatest()
+    const latestCached = await this.cache.getLatest()
+    const latestRemote = await this.remote.getLatest()
+
+    const versionCache = latestCached  ? latestCached.version : '0.0.0'
+    const versionRemote = latestRemote ? latestRemote.version : '0.0.0'
+
+    if(semver.gt(versionCache, versionRemote)){
+      return latestCached
+    }
+
     return latestRemote
   }
 
-  async download(release : IRelease, {writeDetachedMetadata = true} = {} ){
+  async download(release : IRelease, {writePackageData = true, writeDetachedMetadata = true} = {} ){
 
     let pp = 0;
     let onProgress = (p : number) => {
@@ -77,17 +153,26 @@ export default class AppManager extends EventEmitter{
         this.emit('update-progress', release, pn);
       }
     }
-    const releaseData = await this.remote.download(release, onProgress)
+    const packageData = await this.remote.download(release, onProgress)
     const location = path.join(this.cache.cacheDirPath, release.fileName)
 
-    if(writeDetachedMetadata){
-      const detachedMetadataPath = path.join(this.cache.cacheDirPath, release.fileName + '.metadata.json')
-      fs.writeFileSync(detachedMetadataPath, JSON.stringify(release, null, 2))
+    if(writePackageData){
+      if(writeDetachedMetadata){
+        const detachedMetadataPath = path.join(this.cache.cacheDirPath, release.fileName + '.metadata.json')
+        fs.writeFileSync(detachedMetadataPath, JSON.stringify(release, null, 2))
+      }
+      // TODO patch package metadata if it doesn't exist
+      fs.writeFileSync(location, packageData)
+
+    } else{
+      this.emit('update-downloaded', release)
+
+      return {
+        ...release,
+        location: 'memory',
+        data: packageData
+      }
     }
-
-    // TODO patch package metadata if it doesn't exist
-
-    fs.writeFileSync(location, releaseData)
 
     this.emit('update-downloaded', release)
 
@@ -95,6 +180,13 @@ export default class AppManager extends EventEmitter{
       ...release,
       location
     }
+  }
+
+  async hotLoad(release : IRelease | undefined){
+    const hotLoader = new HotLoader(this)
+    // load app to memory and serve from there
+    const hotLoaderUrl = await hotLoader.load(release)
+    return hotLoaderUrl
   }
 
 }
---
+++ updater/HotLoader.ts
@@ -0,0 +1,104 @@
+import url from 'url'
+import { IRelease } from "./api/IRelease";
+import AppManager from "./AppManager";
+//@ts-ignore
+import AdmZip from 'adm-zip'
+
+let addZip : any = null
+let showSplash : any = null
+try {
+  showSplash = require('./electron//ui/show-splash')
+  addZip = require('./electron/electron-zip-support')  
+} catch (error) {
+  console.log('error during require of electron modules', error)
+}
+
+export default class HotLoader {
+  
+  appManager: AppManager;
+
+  constructor(appManager : AppManager){
+    this.appManager = appManager
+  }
+
+  prepare(){
+
+  }
+
+  showSplashscreen(){
+    showSplash(this.appManager)
+  }
+
+  async load(release : IRelease | undefined) {
+
+    if(!release){
+      release = await this.appManager.getLatestRemote() || undefined
+    }
+
+    if(!release){
+      throw new Error('hot load failed: no release was provided or found')
+    }
+
+    // TODO could already be shown while release is retrieved
+    this.showSplashscreen()
+
+    const result = await this.appManager.download(release, {writePackageData: false})
+    // @ts-ignore
+    const { data } = result
+
+    let zip = new AdmZip(data)
+
+    console.log('entries', zip.getEntries().length)
+
+    // TODO make sanity check or throw
+    
+    // allows renderer to access files within zip in memory
+    /**
+     * TODO things to consider:
+     * this is *magic* and magic is usually not a good thing
+     * it will overwrite other interceptors - it seems there can only be one which might be a bug
+     * this will only allow to read from one zip which is probably intended
+     * it will also completely deactivate fs access for files outside the zip which could be a good thing 
+     */
+    addZip(zip)
+
+    const electronUrl = url.format({
+      slashes: true,
+      protocol: 'file:', // even though not 100% correct we are using file and not a custom protocol for the moment
+      pathname: '.zip/index.html', // path does only exist in memory
+    })
+
+    return electronUrl
+  }
+  
+  // async hotLoad(indexHtml) {
+  //   if(showSplash == null){
+  //     throw new Error('Splash cannot be displayed - not running in Electron?')
+  //   }
+
+  //   /*
+  //   let result = {}
+  //   const { location } = app;
+  //   let _result = await request("HEAD", location);
+  //   let headers = _result.headers;
+  //   let zipUrl = location
+  //   if (headers.status === "302 Found" && headers.location) {
+  //     zipUrl = headers.location
+  //   }
+  //   let remoteZip = new RemoteZip(zipUrl)
+  //   addZip(remoteZip)
+  //   */
+
+  //   let electronUrl = url.format({
+  //     slashes: true,
+  //     protocol: 'file:', // even though not 100% correct we are using file and not a custom protocol for the moment
+  //     pathname: '.zip/index.html', // path does only exist in memory
+  //   })
+        
+  //   result.electronUrl = electronUrl
+    
+  //   return result
+
+  // }
+
+}
