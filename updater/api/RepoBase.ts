fbab6cff1cbe7da48031d60045149c44fb9210fb
PhilippLgh <philipp@ethereum.org>
2019-03-03 11:32:17 +0100

    respect channel in release sorting

--- updater/api/RepoBase.ts
+++ updater/api/RepoBase.ts
@@ -2,12 +2,34 @@ import { IRelease, IInvalidRelease, IMetadata, IReleaseExtended } from './IRelea
 import { EventEmitter } from 'events'
 const semver = require('semver')
 
+const REALEASE_CHANNEL : {[index:string] : number} = {
+  dev: -1,
+  ci: -1,
+  alpha: 0,
+  beta: 1,
+  nightly: 2,
+  production: 3,
+  master: 4,
+  release: 4,
+}
+
 class RepoBase extends EventEmitter{
 
   public compareVersions(a : IRelease | IInvalidRelease, b : IRelease | IInvalidRelease){
     if(!('version' in a)) return 1
     if(!('version' in b)) return -1
-    return semver.compare(b.version, a.version)
+    const semComp = semver.compare(b.version, a.version)
+    if(semComp === 0) {
+      console.log('sem', semComp, b.version, b.channel, a.version, a.channel)
+      const channelA = REALEASE_CHANNEL[a.channel || '']
+      const channelB = REALEASE_CHANNEL[b.channel || '']
+      if(!channelA) return 1
+      if(!channelB) return -1
+      if(channelA > channelB) return 1
+      if(channelB > channelA) return -1
+      return 0
+    }
+    return semComp
   }
 
   protected normalizeTag(tag : string){
