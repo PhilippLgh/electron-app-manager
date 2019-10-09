49bfba6945640c69d9a82c8817db00aa46934647
PhilippLgh <philipp@ethereum.org>
2019-10-09 09:49:38 +0200

    add module logic

--- updater/AppManager.ts
+++ updater/AppManager.ts
@@ -1,16 +1,22 @@
 import { EventEmitter } from 'events'
+import url from 'url'
+import fs from 'fs'
 import Cache from './repositories/Cache'
 import { IRepository, IFetchOptionsReleaseList, IFetchOptionsRelease, IDownloadOptions } from './api/IRepository'
 import { getRepository } from './repositories'
-import { compareVersions, getEthpkg } from './util'
-import { IRelease, IInvalidRelease } from './api/IRelease'
+import { compareVersions, getEthpkg, generateHostnameForRelease } from './util'
+import { IRelease, IInvalidRelease, IVerifiedRelease } from './api/IRelease'
 import semver from 'semver'
 import { download } from './lib/downloader'
-import { pkgsign } from 'ethpkg'
+import { pkgsign, ethpkg } from 'ethpkg'
+import { IVerificationResult } from 'ethpkg/dist/IVerificationResult'
 
 export interface IAppManagerOptions {
+  auto?: boolean,
   repository?: string;
   cacheDir?: string;
+  prefix?: string;
+  filter?: Function
 }
 
 export interface IUpdateInfo {
@@ -28,13 +34,19 @@ export const LOGLEVEL = {
 export default class AppManager extends EventEmitter {
 
   loglevel = LOGLEVEL.NORMAL;
-  remote?: IRepository;
-  cache?: Cache;
+  private remote?: IRepository;
+  private cache?: Cache;
+  private filter?: Function; // global release filter function
+  private prefix?: string;
+  private static modules: any = {};
 
   constructor({
+    auto = true,
     repository,
-    cacheDir
-  } : IAppManagerOptions){
+    cacheDir,
+    prefix,
+    filter
+  } : IAppManagerOptions = {}){
 
     super()
 
@@ -44,6 +56,13 @@ export default class AppManager extends EventEmitter {
 
     if (cacheDir) {
       this.cache = new Cache(cacheDir)
+    }
+
+    this.filter = filter
+    this.prefix = prefix
+
+    if (auto) {
+      // TODO start routine here
     }
 
   }
@@ -82,28 +101,31 @@ export default class AppManager extends EventEmitter {
   /**
    * Returns all remote releases
    */
-  private async getReleasesRemote() : Promise<Array<IRelease | IInvalidRelease>> {
+  private async getReleasesRemote(prefix? : string, timeout? : number) : Promise<Array<IRelease | IInvalidRelease>> {
     if (!this.remote) {
       console.warn('Accessing an uninitialized Repository: getReleases()')
       return []
     }
-    return this.remote.getReleases()
+    return this.remote.getReleases(prefix)
   }
 
   /**
    * Returns all releases. Combines cached and remote releases
    * @param fetchOptions 
    */
-  public async getReleases({
+  public async getAllReleases({
     sort = true,
     onlyCache = false,
     onlyRemote = false,
     version = undefined,
-    filterInvalid = true
-  } : IFetchOptionsReleaseList) : Promise<Array<IRelease | IInvalidRelease>> {
+    filterInvalid = true,
+    prefix,
+    timeout = (30 * 1000)
+  } : IFetchOptionsReleaseList = {}) : Promise<Array<IRelease | IInvalidRelease>> {
 
     const cachedReleases = onlyRemote ?  [] : await this.getReleasesCached()
-    const remoteReleases = onlyCache ? [] : await this.getReleasesRemote()
+    prefix = this.prefix || prefix
+    const remoteReleases = onlyCache ? [] : await this.getReleasesRemote(prefix, timeout)
 
     let releases = [
       ...cachedReleases,
@@ -121,32 +143,52 @@ export default class AppManager extends EventEmitter {
       })
     }
 
-    const invalid = releases.filter(release => ('error' in release))
+    const invalid = releases.filter(release => ('error' in release && release.error))
     if (invalid.length > 0) {
       this.log(LOGLEVEL.WARN, `detected ${invalid.length} corrupted releases found`)
       this.log(LOGLEVEL.VERBOSE, invalid.map(r => r.error).join('\n\n'))
     }
 
     if (filterInvalid) {
-      releases = releases.filter(release => !('error' in release))
+      releases = releases.filter(release => !('error' in release && release.error))
+    }
+
+    // apply global filter
+    if (this.filter) {
+      // catch errors in user-defined filter
+      try {
+        // FIXME
+        // @ts-ignore 
+        releases = releases.filter(this.filter)
+      } catch (error) {
+        
+      }
     }
 
     if (sort) {
-      return releases.sort(compareVersions)
+      releases =  releases.sort(compareVersions)
     }
 
     return releases
   }
 
   /**
-   * 
-   * @param fetchOptions
+   * @deprecated too close to "getRelease" -> renamed to getAllReleases
+   * @param options 
    */
-  public async getLatest({
+  public async getReleases(options: IFetchOptionsReleaseList = {}) : Promise<Array<IRelease | IInvalidRelease>> {
+    return this.getAllReleases(options)
+  }
+
+  /**
+   * Retrieves a single release 
+   * @param options
+   */
+  public async getRelease({
     onlyCache = false,
     onlyRemote = false,
     version = undefined,
-  } : IFetchOptionsRelease) : Promise<IRelease | undefined> {
+  } : IFetchOptionsRelease = {}) : Promise<IRelease | undefined> {
 
     const releases = await this.getReleases({
       onlyCache,
@@ -191,19 +233,134 @@ export default class AppManager extends EventEmitter {
   }
 
   /**
+   * Retrieves a single release
+   * @deprecated renamed to getRelease
+   * @param options 
+   */
+  public async getLatest(options : IFetchOptionsRelease) : Promise<IRelease | undefined> {
+    return this.getRelease(options)
+  }
+
+  /**
+   * A module is a downloaded and verified package that is kept in memory
+   * and addressable via hostname
+   * We can read files from the package with getResource
+   */
+  private async registerModule(release : IVerifiedRelease) : Promise<string> {
+    const hostname = await generateHostnameForRelease(release)
+    AppManager.modules[hostname] = release
+    return hostname
+  }
+
+  private getModule(hostname : string) : IVerifiedRelease {
+    return AppManager.modules[hostname]
+  }
+
+  /**
    * 
    */
-  public async load(release : IRelease, protocol : string) : Promise<string | undefined> {
-    if (release.remote) {
-
+  public async load(releaseOrInfo : string | undefined /*| IRelease*/, protocol? : string) : Promise<string | undefined> {
+    // release string can be path, url or version
+    let targetVersion = undefined
+    if (typeof releaseOrInfo === 'string') {
+      targetVersion = releaseOrInfo
     }
-    return
+
+    // else nothing in cache -> try to fetch from remote
+    let release = await this.getRelease({
+      onlyRemote: true, 
+      version: targetVersion,
+      /* FIXME
+      download: true,
+      downloadOptions: {
+        writePackageData: true, // will write data to cache
+        onProgress: (progress, release) => onProgress(release, progress)
+      }
+      */
+    })
+
+    // release could not be found
+    if(!release) {
+      return undefined
+    }
+
+    // download and verify package
+    const verifiedRelease = await this.download(release)
+    const hostname = await this.registerModule(verifiedRelease)
+
+    // const protocol = 'package:'
+    const appUrl = url.format({
+      slashes: true,
+      protocol,
+      pathname: `${hostname}/index.html`
+    })
+
+    return appUrl
+  }
+
+  public async getResource(verifiedReleaseOrHostname : string | IVerifiedRelease, resourcePath : string) : Promise<Buffer | undefined> {
+    // TODO implement and check verification policy
+    let verifiedRelease = undefined
+    if (typeof verifiedReleaseOrHostname=== 'string') {
+      verifiedRelease = this.getModule(verifiedReleaseOrHostname)
+    } else {
+      verifiedRelease = verifiedReleaseOrHostname
+    }
+    if (!verifiedRelease) {
+      return undefined
+    }
+    const data = verifiedRelease.data
+    // initialize ethpkg
+    const pkg = await ethpkg.getPackage(data)
+    const entry = await pkg.getEntry(resourcePath)
+    if (!entry) {
+      // throw new Error(`Entry not found: ${resourcePath}`)
+      return undefined
+    }
+    const content = await entry.file.readContent()
+    // TODO memoize
+    return content
+  }
+
+  /**
+   * @deprecated
+   * @param hostname 
+   */
+  public async getEntry(release : IRelease, resourcePath : string) : Promise<Buffer | undefined> {
+    let verifiedRelease = undefined
+    if (!release.remote) {
+      const data = fs.readFileSync(release.location)
+      const verificationResult = {} //FIXME await this.verifyReleaseData(data)
+      verifiedRelease = {
+        ...release,
+        data,
+        verificationResult
+      }
+    }
+    if (!verifiedRelease) {
+      throw new Error('Not a verified release')
+    }
+    // @ts-ignore
+    return this.getResource(verifiedRelease, resourcePath)
+  }
+
+  async getEntries(release : IRelease) {
+    if (!this.cache) return []
+    return this.cache.getEntries(release)
+  }
+
+  private async verifyReleaseData(data : Buffer) : Promise<IVerificationResult> {
+    // verify package signature: TODO we can enforce a policy here that invalid
+    // packages are not even written to disk
+    const pkg = await getEthpkg(data)
+    const verificationResult = await pkgsign.verify(pkg!)
+    return verificationResult
   }
 
   public async download(release : IRelease, {
     onProgress = (progress : number, release?: IRelease) => {},
     writePackageData = true
-  } : IDownloadOptions = {}) : Promise<IRelease> {
+  } : IDownloadOptions = {}) : Promise<IVerifiedRelease> {
 
     // wrap onProgress
     let pp = 0;
@@ -223,10 +380,7 @@ export default class AppManager extends EventEmitter {
     const { location } = release
     const packageData = await download(location, _onProgress)
 
-    // verify package signature: TODO we can enforce a policy here that invalid
-    // packages are not even written to disk
-    const pkg = await getEthpkg(packageData)
-    const verificationResult = await pkgsign.verify(pkg!)
+    const verificationResult = await this.verifyReleaseData(packageData)
 
     // 
     if (writePackageData) {
